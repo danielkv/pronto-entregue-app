9725dd18ca997eacddaec6c82e30a63beeab14ce
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-02-18 20:58:18 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,31 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View, ActivityIndicator } from 'react-native';
 
+import { useQuery } from '@apollo/react-hooks';
 import { DrawerActions } from '@react-navigation/routers';
 import { LinearGradient } from 'expo-linear-gradient';
 
-import { useTheme, Icon, Typography } from "../../react-native-ui";
+import { useTheme, Icon, Typography, Avatar, IconButton } from "../../react-native-ui";
+import { useLoggedUserId } from '../../utils/hooks';
+import { RigthContent } from './styles';
+
+import { GET_USER } from '../../graphql/users';
+
 
 export default function  AppHeader({ variant='solid', previous, scene, navigation }) {
 	const theme = useTheme();
 	const { options } = scene.descriptor;
 	const { params } = scene.route;
 
+	const loggedUserId = useLoggedUserId();
+	const { data: { user = null } ={}, loading: loadingUser } = useQuery(GET_USER, { variables: { id: loggedUserId } })
+
 	const ContainerComponent = variant === 'transparent'
 		? LinearGradient
 		: View
 
-	const iconsColor = variant === 'transparent' ? '#fff' : theme.palette.gray;
+	const iconsColor = variant === 'transparent' ? '#fff' : theme.palette.background.dark;
 
 	const title = params && params.headerTitle
 		? params.headerTitle
@@ -55,6 +64,17 @@ export default function  AppHeader({ variant='solid', previous, scene, navigatio
 				)}
 				
 			{Boolean(title) && <Typography variant='h3' style={{ color: iconsColor }}>{title}</Typography>}
+
+			<RigthContent>
+				<IconButton onPress={()=>{}} icon={{ name: 'search', color: iconsColor }} />
+				<IconButton onPress={()=>{}} icon={{ name: 'bell', color: iconsColor }} />
+				{loadingUser
+					? <ActivityIndicator />
+					: <Avatar
+						source={{ uri: 'https://s3.amazonaws.com/37assets/svn/1065-IMG_2529.jpg' }}
+						alt={user.fullName}
+					/>}
+			</RigthContent>
 		</ContainerComponent>
 	)
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const RigthContent = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
