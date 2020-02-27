22064608c86e9023de6b0051bdecbec02f803e02
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-02-27 19:13:40 -0300

    root routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { View } from 'react-native'
 import { useSafeArea } from 'react-native-safe-area-context';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 import LoadingBlock from './components/LoadingBlock';
 
@@ -12,8 +11,6 @@ import MainRoutes from './routes/main';
 import { useInitialize } from './services/init';
 import NavigatorTheme from './theme/navigator';
 
-const Stack = createStackNavigator();
-
 export default function SplashScreen() {
 	const { loading, loggedUserId } = useInitialize();
 	const insets = useSafeArea();
@@ -21,19 +18,14 @@ export default function SplashScreen() {
 	return (
 		<View style={{ flex: 1, paddingTop: insets.top, paddingBottom: insets.bottom }}>
 			<NavigationContainer theme={NavigatorTheme}>
-				<Stack.Navigator
-					initialRouteName='Loading'
-					headerMode='none'
-				>
-					{
-						// eslint-disable-next-line no-nested-ternary
-						loading
-							? <Stack.Screen name='Loading' component={LoadingBlock} />
-							: !loggedUserId
-								? <Stack.Screen name='LoginRoutes' component={AuthenticationRoutes} />
-								: <Stack.Screen name='AppRoutes' component={MainRoutes} />
-					}
-				</Stack.Navigator>
+				{
+					// eslint-disable-next-line no-nested-ternary
+					loading
+						? <LoadingBlock />
+						: !loggedUserId
+							? <AuthenticationRoutes />
+							: <MainRoutes />
+				}
 			</NavigationContainer>
 		</View>
 	);
