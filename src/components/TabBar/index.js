ee2075152feab4f40d1130ac1a5ca9edd2f274cf
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-03-27 23:35:19 -0300

    selected tab

--- src/components/TabBar/index.js
+++ src/components/TabBar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { useQuery } from '@apollo/react-hooks';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 
 import { Badge } from '../../react-native-ui';
 import MenuItem from './MenuItem';
@@ -11,17 +11,18 @@ import  { GET_CART } from '../../graphql/cart';
 
 export default function TabBar(props) {
 	const navigation = useNavigation();
+	const { name: routeName } = useRoute();
 
 	const { data: { cartItems } } = useQuery(GET_CART);
 
 	return (
 		<Container {...props} >
-			<MenuItem selected icon='home' label='Home' onPress={()=>navigation.navigate('HomeRoutes', { screen: 'FeedScreen' })} />
+			<MenuItem selected={routeName === 'HomeRoutes'} icon='home' label='Home' onPress={()=>navigation.navigate('HomeRoutes', { screen: 'FeedScreen' })} />
 			<Badge color='primary' badgeContent={cartItems.length} style={{ badge: { marginTop: 5, marginRight: 12 } }}>
-				<MenuItem icon='shopping-bag' label='Cesta' onPress={()=>navigation.navigate('CartRoutes', { screen: 'CartScreen' })} />
+				<MenuItem selected={routeName === 'CartRoutes'} icon='shopping-bag' label='Cesta' onPress={()=>navigation.navigate('CartRoutes', { screen: 'CartScreen' })} />
 			</Badge>
-			<MenuItem icon='list' label='Meus Pedidos' onPress={()=>navigation.navigate('OrderRoutes', { screen: 'OrderListScreen' })} />
-			<MenuItem icon='settings' label='Opções' screenName='HomeRoutes' />
+			<MenuItem selected={routeName === 'OrderRoutes'} icon='list' label='Meus Pedidos' onPress={()=>navigation.navigate('OrderRoutes', { screen: 'OrderListScreen' })} />
+			<MenuItem selected={routeName === 'SettingsRoutes'} icon='settings' label='Opções' screenName='HomeRoutes' />
 		</Container>
 	);
 }
