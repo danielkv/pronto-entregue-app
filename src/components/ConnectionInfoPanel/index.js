9ad2f9fa09899ea4c637b21fee7b2b32222287ea
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-19 15:32:33 -0300

    Painel erro conexão - melhoria

--- src/components/ConnectionInfoPanel/index.js
+++ src/components/ConnectionInfoPanel/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { View, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StatusBar, Platform } from 'react-native';
 
 import { useNetInfo } from "@react-native-community/netinfo";
+import { BlurView } from 'expo-blur';
 
 import { Icon, useTheme, Typography, Chip } from '../../react-native-ui';
 
 // import { Container } from './styles';
+let timeOut = null;
 
 export default function ConnectionInfoPanel() {
 	const { palette } = useTheme();
-	const netInfo = useNetInfo();
+	const { isConnected, isInternetReachable } = useNetInfo();
+	const [connected, setConnected] = useState(true);
 
-	if (netInfo.isConnected && netInfo.isInternetReachable) return false;
+	useEffect(()=>{
+		if (timeOut) clearTimeout(timeOut);
+
+		if (!isConnected || !isInternetReachable) {
+			timeOut = setTimeout(()=>setConnected(false), 3000);
+		} else {
+			setConnected(true);
+		}
+	}, [isConnected, isInternetReachable])
+
+
+	if (connected) return false;
 
 	return (
-		<View
+		<BlurView
 			style={{
 				position: 'absolute',
 				top: 0,
@@ -23,16 +37,17 @@ export default function ConnectionInfoPanel() {
 				right: 0,
 				width: '100%',
 				height: '100%',
-				backgroundColor: 'rgba(0,0,0,0.7)',
 				justifyContent: 'center',
 				alignItems: 'center',
 				padding: 35,
 			}}
+			tint='dark'
+			intensity={Platform.OS === 'ios' ? 100 : 150}
 		>
 			<StatusBar backgroundColor='transparent' translucent={true} barStyle='light-content' />
 			<Icon name='wifi-off' color={palette.secondary.main} size={50} />
 			<Chip color='primary' style={{ root: { alignSelf: 'auto', marginTop: 10 } }}>Falha na conexão</Chip>
-			<Typography variant='h5' style={{ color: '#fff', textAlign: "center", marginTop: 10 }}>O App ficará disponível assim que sua internet for ativada</Typography>
-		</View>
+			<Typography variant='h5' style={{ color: '#fff', textAlign: "center", marginTop: 10, textShadowRadius: 8, textShadowColor: '#000000', textShadowOffset: { x: 2, y: 2 } }}>O App ficará disponível assim que sua conexão com a internet for estabelecida</Typography>
+		</BlurView>
 	);
 }
