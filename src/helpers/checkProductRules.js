08c6fd64a09fcae1259c1de18e0321d0874b1eaa
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-08-06 23:42:13 -0300

    allowBuyClosed

--- src/helpers/checkProductRules.js
+++ src/helpers/checkProductRules.js
@@ -12,6 +12,9 @@ import { GET_CART } from '../graphql/cart';
 export default function checkProductRules({ company, product }, force) {
 	const { cartItems, cartCompany } = client.readQuery({ query: GET_CART });
 
+	// check if there is other company in cart
+	if (!force && (cartCompany !== null && cartCompany?.id !== company.id)) throw new CartValidationError('Já existem itens de outro estabelecimento na sua cesta.', 'Quer mesmo limpar sua cesta e adicionar esse item?');
+
 	// check is Company is is open
 	const isOpen = company.nextClose ? moment(company.nextClose).isSameOrAfter() : false;
 	const allowBuyClosed = company.allowBuyClosed;
@@ -24,15 +27,15 @@ export default function checkProductRules({ company, product }, force) {
 				throw new Error('Esse estabelecimento aceita pedidos enquanto está fechado apenas para produtos sob encomenda');
 		}
 	}
-	
-	// check if there is other company in cart
-	if (!force && (cartCompany !== null && cartCompany?.id !== company.id)) throw new CartValidationError('Já existem itens de outro estabelecimento na sua cesta.', 'Quer mesmo limpar sua cesta e adicionar esse item?');
 
 	// check if there is any schedulable product in cart
-	const schedulableProducts = getSchedulableProducts(cartItems);
-	if (!force && (!product.scheduleEnabled && schedulableProducts.length
-		|| product.scheduleEnabled && cartItems.length > schedulableProducts.length)) {
-		throw new CartValidationError('Há produtos sob encomenda na cesta', 'Esse item não pode ser adicionado em uma cesta que já tem itens sob encomenda. Deseja limpar a cesta e adicionar esse item?');
+	if (!force) {
+		const schedulableProducts = getSchedulableProducts(cartItems);
+		if (!product.scheduleEnabled && schedulableProducts.length)
+			throw new CartValidationError('Há produtos sob encomenda na cesta', 'Esse item não pode ser adicionado em uma cesta que já tem itens sob encomenda. Deseja limpar a cesta e adicionar esse item?');
+
+		if (product.scheduleEnabled && cartItems.length > schedulableProducts.length)
+			throw new CartValidationError('Esse produto é feito sob encomenda', 'Não há itens sob encomenda em sua cesta. Deseja limpar a cesta e adicionar esse item?');
 	}
 
 	// check if there is same items in cart
